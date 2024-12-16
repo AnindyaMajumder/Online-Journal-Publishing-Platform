e2315d2a01066a4611a0df2abe1c89b2bd953e24
Anindya Majumder <anindyamajumder.54@gmail.com>
2024-12-16 13:22:09 +0600

    Refactored

--- src/main/java/com/groupthirteen/nais_journal/controller/AuthController.java
+++ src/main/java/com/groupthirteen/nais_journal/controller/AuthController.java
@@ -1,51 +1,39 @@
 package com.groupthirteen.nais_journal.controller;
 
-import com.groupthirteen.nais_journal.Repository.UserEntryRepo;
 import com.groupthirteen.nais_journal.model.UserEntity;
-import com.groupthirteen.nais_journal.security.JwtUtils;
+import com.groupthirteen.nais_journal.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
 public class AuthController {
-    private final UserEntryRepo userEntryRepo;
-    private final PasswordEncoder passwordEncoder;
-    private final AuthenticationManager authenticationManager;
-    private final JwtUtils jwtUtils;
-
-    public AuthController(UserEntryRepo userEntryRepo, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, JwtUtils jwtUtils) {
-        this.userEntryRepo = userEntryRepo;
-        this.passwordEncoder = passwordEncoder;
-        this.authenticationManager = authenticationManager;
-        this.jwtUtils = jwtUtils;
-    }
+    @Autowired
+    private AuthService authService;
 
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody UserEntity user){
-        if(userEntryRepo.findByUsername(user.getUsername()) != null){
-            return ResponseEntity.badRequest().body("Username already exists");
+        boolean isRegistered = authService.addUser(user);
+        if(isRegistered){
+            return ResponseEntity.ok("User registered successfully");
         }
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setROLE("USER");
-        userEntryRepo.save(user);
-
-        return ResponseEntity.ok().body("User registered successfully");
+        else {
+            return ResponseEntity.badRequest().body("User already exists");
+        }
     }
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody UserEntity user){
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword())
-        );
-        String token = jwtUtils.generateToken(authentication.getName());
-        return ResponseEntity.ok(token);
+        String token = authService.authenticate(user);
+        if(!token.isBlank()){
+            return ResponseEntity.ok(token);
+        }
+        return ResponseEntity.badRequest().body("Invalid username or password");
     }
+
+//    @PutMapping("/forget-pass")
+//    public ResponseEntity<?> forgetPass(@RequestBody UserEntity user){
+//
+//    }
 }
