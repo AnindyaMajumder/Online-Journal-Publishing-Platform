9a1120b72196e02bec06568e980408afa055297e
Anindya Majumder <anindyamajumder.54@gmail.com>
2024-12-16 13:21:41 +0600

    Updated

---
+++ src/main/java/com/groupthirteen/nais_journal/service/AuthService.java
@@ -0,0 +1,56 @@
+package com.groupthirteen.nais_journal.service;
+
+import com.groupthirteen.nais_journal.Repository.UserEntryRepo;
+import com.groupthirteen.nais_journal.model.UserEntity;
+import com.groupthirteen.nais_journal.security.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+    @Autowired
+    private UserEntryRepo userEntryRepo;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private JwtUtils jwtUtils;
+
+//    public AuthService(UserEntryRepo userEntryRepo, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, JwtUtils jwtUtils) {
+//        this.userEntryRepo = userEntryRepo;
+//        this.passwordEncoder = passwordEncoder;
+//        this.authenticationManager = authenticationManager;
+//        this.jwtUtils = jwtUtils;
+//    }
+
+    public boolean addUser(UserEntity user) {
+        if(userEntryRepo.findByUsername(user.getUsername()) != null) {
+            return false;
+        }
+        else {
+            user.setFirstName(user.getFirstName());
+            user.setLastName(user.getLastName());
+            user.setEmail(user.getEmail());
+
+            user.setUsername(user.getUsername());
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            user.setROLE(user.getROLE());
+
+            userEntryRepo.save(user);
+
+            return true;
+        }
+    }
+
+    public String authenticate(UserEntity user) throws AuthenticationException {
+
+            Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+            return jwtUtils.generateToken(authentication.getName());
+    }
+}
