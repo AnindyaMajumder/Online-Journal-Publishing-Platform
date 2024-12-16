170f97347d869921b873bcc2dce350044283cae8
Anindya Majumder <anindyamajumder.54@gmail.com>
2024-12-16 13:19:01 +0600

    Updated

--- src/main/java/com/groupthirteen/nais_journal/NaisJournalApplication.java
+++ src/main/java/com/groupthirteen/nais_journal/NaisJournalApplication.java
@@ -2,7 +2,6 @@ package com.groupthirteen.nais_journal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class NaisJournalApplication{
