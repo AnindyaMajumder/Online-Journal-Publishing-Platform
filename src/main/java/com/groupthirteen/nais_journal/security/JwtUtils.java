8caf26426be6ffb92f91b21bbcf638e9fed799cd
Anindya Majumder <anindyamajumder.54@gmail.com>
2024-12-17 01:04:07 +0600

    Token blacklisting added

--- src/main/java/com/groupthirteen/nais_journal/security/JwtUtils.java
+++ src/main/java/com/groupthirteen/nais_journal/security/JwtUtils.java
@@ -4,29 +4,41 @@ import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 @Component
 public class JwtUtils {
     private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-    private final long tokenValidity = 7 * 24 * 60 * 60 * 1000; // 7days
+    private final Set<String> blacklistedTokens = new HashSet<>();
 
     public String generateToken(String username) {
+        long tokenValidity = 24 * 60 * 60 * 1000;
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis()+tokenValidity))
+                .setExpiration(new Date(System.currentTimeMillis()+ tokenValidity))
                 .signWith(key)
                 .compact();
     }
 
+    public boolean isBlacklisted(String token) {
+        return blacklistedTokens.contains(token);
+    }
+
+    public void blacklistToken(String token) {
+        blacklistedTokens.add(token);
+    }
     public boolean validateToken(String token) {
         try {
             Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
-            return true;
+
+            return !isBlacklisted(token);
         }
         catch (JwtException | IllegalArgumentException e) {
             return false;
