8ea16ce21535e965d3432e0febee12fbc67099e0
Anindya Majumder <anindyamajumder.54@gmail.com>
2024-12-17 01:05:11 +0600

    Disable default logout of SS6

--- src/main/java/com/groupthirteen/nais_journal/config/SecurityConfig.java
+++ src/main/java/com/groupthirteen/nais_journal/config/SecurityConfig.java
@@ -6,9 +6,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
@@ -20,11 +22,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
         return http.csrf(csrf->csrf.disable())
-                .securityMatcher("/**")
                 .authorizeHttpRequests(auth->auth
-                        .requestMatchers("/","/login","/register").permitAll()
+                        .requestMatchers("/","/login","/register","/logout").permitAll()
                         .anyRequest().authenticated()
-                ).build();
+                )
+                .logout(logout->logout.disable())
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 
     @Bean
