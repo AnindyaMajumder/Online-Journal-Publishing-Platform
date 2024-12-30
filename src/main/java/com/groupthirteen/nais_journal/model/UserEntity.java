fdc424866e307c60df25344fc12070af44d5855b
Anindya Majumder <anindyamajumder.54@gmail.com>
2024-12-30 23:13:14 +0600

    Updated

--- src/main/java/com/groupthirteen/nais_journal/model/UserEntity.java
+++ src/main/java/com/groupthirteen/nais_journal/model/UserEntity.java
@@ -6,10 +6,8 @@ import lombok.NonNull;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.List;
 
 @Document (collection = "Users")
 @Data
